0804f232248946b87b08b1724e5a5ebd9c12f9ba
Yue Zhao <yzhao062@gmail.com>
2020-04-30 17:10:41 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # OD-Econometrics
 Outlier Detection and Removal for Econometrics Models
 
+----
+
+## Motivation
+
+Anomaly detection is a major machine learning technique aiming to identify the deviant samples from the general data distribution. 
+It has many key business applications in various fields, including intrusion detection, mechanical fault detection, credit card default prediction, and terrorism event detection. 
+Other than simple outlier detection methods, e.g., removing the samples which are more than two standard deviations from the sample mean, many more advanced and complex approaches are proposed in the past several decades. 
+However, their usages in traditional econometrics field is still rare, although many models are sensitive to outliers. It is acknowledged that the presence of outliers may skew the estimation result heavily. 
+In this study, we will empirically examine the effect of latest outlier detection algorithms on classical econometrics models.
+
+
+
+----
+
+### Note (April 30th)
+
 This is an ongoing project. Use with Caution :)
---
+++ main_analysis.py
@@ -0,0 +1,100 @@
+import os
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+
+from statsmodels.discrete.discrete_model import Probit, Logit, \
+    GeneralizedPoisson, Poisson
+import matplotlib.pyplot as plt
+
+"""
+Attend
+# Estimate a linear regression relating atndrte to ACT, priGPA, frosh, and soph; 
+compute the usual OLS standard errors. Interpret the coe‰cients on ACT and priGPA. 
+Are any fitted values outside the unit interval?
+
+http://fmwww.bc.edu/ec-p/data/wooldridge/attend.des
+
+Smoke
+# Use a linear regression model to explain cigs, the number of cigarettes smoked
+per day. Use as explanatory variables logðcigpricÞ, logðincomeÞ, restaurn, white,
+educ, age, and age2. Are the price and income variables significant? Does using
+heteroskedasticity-robust standard errors change your conclusions?
+
+http://fmwww.bc.edu/ec-p/data/wooldridge/smoke.des
+
+
+BWGHT
+# Define a binary variable, smikes, if the women smokes during the pregnancy
+estimate a probit model relating smokes to mothereduc, white, and log(faminc)
+http://fmwww.bc.edu/ec-p/data/wooldridge/bwght.des
+
+
+Grogger
+Define a binary variable, say arr86, equal to unity if a man was arrested at least
+once during 1986, and zero otherwise. Estimate an LPM relating arr86 to pcnv, avgsen,
+tottime, ptime86, inc86, black, hispan, and born60. Report the usual and heteroskedasticity-
+robust standard errors. 
+http://fmwww.bc.edu/ec-p/data/wooldridge2k/GROGGER.DES
+
+"""
+
+dta_lists = [
+    # "attend.dta",  
+    # "smoke.dta",
+    "bwght.dta",
+    # "grogger.dta",
+]
+
+for i in range(len(dta_lists)):
+    df = pd.read_stata(os.path.join("data", dta_lists[i]))
+    print(df.columns)
+
+    # attend    
+    # Y = df['atndrte']
+    # X = df[['priGPA', 'ACT', 'frosh', 'soph']] 
+
+    # smoke
+    df['smokes'] = (df['cigs'] >= 1).astype(int)
+    Y = df['smokes']
+    X = df[['motheduc', 'lfaminc', 'white']]
+    X = X.fillna(X.mean())
+
+    # bwght
+    # Y = df['cigs']
+    # X = df[['educ', 'cigpric', 'white', 'age', 'income', 'restaurn',
+    #     'lincome', 'agesq', 'lcigpric']] 
+
+    # grogger
+    # df['arr86'] = (df['narr86']>=1).astype(int)
+    # Y = df['arr86']
+    # X = df[['pcnv', 'avgsen', 'tottime', 'ptime86', 'inc86', 'black', 'hispan', 'born60']]
+    # X = sm.add_constant(X)
+
+    # general OLS
+    # https://www.statsmodels.org/stable/generated/statsmodels.regression.linear_model.OLS.html
+    # model=sm.OLS(Y, X.astype(float))
+
+    # robust regression 
+    # https://www.statsmodels.org/stable/generated/statsmodels.robust.robust_linear_model.RLM.html
+    # model=sm.RLM(Y, X.astype(float))
+
+    # probit model 
+    # https://www.statsmodels.org/stable/generated/statsmodels.discrete.discrete_model.Probit.html
+    model = Probit(Y, X.astype(float))
+
+    # logit model 
+    # https://www.statsmodels.org/stable/generated/statsmodels.discrete.discrete_model.Logit.html
+    # model = Logit(Y, X.astype(float))
+
+    # poisson model 
+    # https://www.statsmodels.org/stable/generated/statsmodels.formula.api.poisson.html
+    # model = Poisson(Y, X.astype(float))
+
+    final_model = model.fit()
+    results_summary = final_model.summary()
+    print(results_summary)
+    results_as_html = results_summary.tables[1].as_html()
+    result_df = pd.read_html(results_as_html, header=0, index_col=0)[0]
+
+    print(result_df.to_latex())
